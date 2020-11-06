1eeb00a10dfd8387ba120069c41bde296e269005
vibueno <victor@localhost>
2020-11-06 11:50:13 +0100

    Added navigation template

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,16 @@
+/**
+ * @module constants
+ */
 
+const TPL_MENU = document.getElementById("template-menu");
+
+const HEADER_NAV = document.getElementById("header-nav");
+const FOOTER_NAV = document.getElementById("footer-nav");
+const SIDE_NAV = document.getElementById("side-nav");
+
+
+export {
+  TPL_MENU,
+  FOOTER_NAV,
+  HEADER_NAV,
+  SIDE_NAV };
--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-import { } from './modules/classes.js';
+import {
+  TPL_MENU,
+  FOOTER_NAV,
+  HEADER_NAV,
+  SIDE_NAV } from './constants.js';
+
+/**
+ * @description Shows the menus based on a template.
+ */
+function showMenus() {
+  const HEADER_MENU = TPL_MENU.content.cloneNode(true);
+  HEADER_NAV.appendChild(HEADER_MENU);
+
+  const FOOTER_MENU = TPL_MENU.content.cloneNode(true);
+  FOOTER_NAV.appendChild(FOOTER_MENU);
+
+  const SIDE_MENU = TPL_MENU.content.cloneNode(true);
+  SIDE_NAV.appendChild(SIDE_MENU);
+
+}
+
+/*
+ *
+ * Initial calls
+ *
+ */
+
+window.onload = function(){
+  showMenus();
+}
